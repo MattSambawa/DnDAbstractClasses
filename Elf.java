86cb7028dee16d15231cb855656c785e79bef688
MattSambawa <48026938+MattSambawa@users.noreply.github.com>
2022-04-08 16:33:49 -0700

    Create Elf.java

---
+++ Elf.java
@@ -0,0 +1,29 @@
+public class Elf extends GameCharacter {
+
+  public Elf(String name, String charClass, String alignment, int gold, int hitPoints, int armorClass, Weapon weapon1, Weapon weapon2) {
+    super(name, charClass, alignment, gold, hitPoints, armorClass, weapon1, weapon2);
+  }
+
+  public void assist(GameCharacter gc) {
+    gc.setHitPoints(gc.getHitPoints() + 5);
+    gc.setGold(gc.getGold() + 10);
+  }
+  
+  public boolean attack(GameCharacter gc) {
+    if (gc.getArmorClass() == 1) {
+      gc.setHitPoints(gc.getHitPoints() - 5);
+      return true;
+    } else if (gc.getArmorClass() == 2) {
+      gc.setHitPoints(gc.getHitPoints() - 5);
+      return true;
+    } else if (gc.getArmorClass() == 3) {
+      gc.setHitPoints(gc.getHitPoints() - 3);
+      return true;
+    } else if (gc.getArmorClass() < 1) {
+      gc.setHitPoints(gc.getHitPoints() - 15);
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
